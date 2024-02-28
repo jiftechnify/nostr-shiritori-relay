350860acaeee9e5feff50c91d0a46e5c23fb1347
jiftechnify <jiftech.stlfy@gmail.com>
2024-02-29 01:53:06 +0900

    add "point" command to rtp inspector

--- ritrin/rtp_inspect.ts
+++ ritrin/rtp_inspect.ts
@@ -3,6 +3,7 @@ import {
   lastShiritoriAcceptedAtPerAuthorKey,
   lastShiritoriConnectionKey,
 } from "./ritrin_point/grant.ts";
+import { RitrinPointTransaction } from "./ritrin_point/model.ts";
 
 type LastShiritoriConnectionRecord = {
   pubkey: string;
@@ -17,14 +18,20 @@ Commands:
 
   last-accepted <pubkey>         Show the last shiritori accepted time of the pubkey
   last-srtr-conn                 Show the last shiritori connection record
-  point-txs <pubkey> [date-str]  Show all the point transactions of the pubkey.
+  point <pubkey> [date-str]      Show the total point amount of the pubkey.
+	                               If date-str is given, show the total of the date.
+				                         date-str format: yyyy-mm-dd or "today"
+  point-tx <pubkey> [date-str]   Show all the point transactions of the pubkey.
                                  If date-str is given, show the transactions of the date.
+                                 date-str format: yyyy-mm-dd or "today"
 `;
 
 const showUsageAndExit = () => {
   console.log(usage);
   Deno.exit(1);
 };
+
+const dateStrOfToday = () => Temporal.Now.plainDateISO().toString();
 
 if (import.meta.main) {
   const kv = await Deno.openKv("../resource/rtp.db");
@@ -52,22 +59,52 @@ if (import.meta.main) {
       console.log(res.value);
       break;
     }
-    case "point-txs": {
+    case "point": {
       if (Deno.args[1] === undefined) {
         console.error("missing pubkey");
         showUsageAndExit();
       }
       const [pubkey, dateStr] = Deno.args.slice(1);
       const rtpTxRepo = new RitrinPointTxRepo(kv);
-      const txs = dateStr
+
+      const calcTotalPts = (txs: RitrinPointTransaction[]) =>
+        txs.reduce((sum, tx) => sum + tx.amount, 0);
+
+      if (dateStr !== undefined) {
+        const d = dateStr === "today" ? dateStrOfToday() : dateStr;
+        const txs = await rtpTxRepo.findAllByPubkeyWithinDay(pubkey, d);
+        console.log(
+          `${pubkey}'s total ritrin points (${d}): ${calcTotalPts(txs)}`,
+        );
+        break;
+      }
+      const txs = await rtpTxRepo.findAllByPubkey(pubkey);
+      console.log(
+        `${pubkey}'s total ritrin points (all-time): ${calcTotalPts(txs)}`,
+      );
+      break;
+    }
+    case "point-tx": {
+      if (Deno.args[1] === undefined) {
+        console.error("missing pubkey");
+        showUsageAndExit();
+      }
+      const [pubkey, dateStr] = Deno.args.slice(1);
+      const rtpTxRepo = new RitrinPointTxRepo(kv);
+      const txs = dateStr === "today"
+        ? await rtpTxRepo.findAllByPubkeyWithinDay(
+          pubkey,
+          dateStrOfToday(),
+        )
+        : dateStr !== undefined
         ? await rtpTxRepo.findAllByPubkeyWithinDay(pubkey, dateStr)
         : await rtpTxRepo.findAllByPubkey(pubkey);
 
       if (txs.length === 0) {
-        console.log("no records found")
-	break;
+        console.log("no records found");
+        break;
       }
-      console.log(`${txs.length} records found`)
+      console.log(`${txs.length} records found`);
       for (const tx of txs) {
         console.log(tx);
       }
@@ -79,4 +116,3 @@ if (import.meta.main) {
     }
   }
 }
-
